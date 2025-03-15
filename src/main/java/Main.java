8f640d467491f7743cce63fb141733c9a35d405e
alancj731 <alanchenjian@gmail.com>
2025-03-14 22:20:50 -0500

    step 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,20 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            String[] parts = parseInput(input);
+            String command = parts[0];
+            String arg = parts[1];
+            if (command.equals("exit")) {
+                break;
+            }
             System.out.println(input + ": command not found");
         }
+    }
+
+    private static String[] parseInput(String input) {
+        String[] parts = input.split(" ", 2);
+        String command = parts.length > 0 ? parts[0] : "";
+        String arg = parts.length > 1 ? parts[1] : "";
+        return new String[]{command, arg};
     }
 }
