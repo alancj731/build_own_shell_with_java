7f13db3c43bd9dad2cd19a9dbb9b2e9bad712f4a
alancj731 <alanchenjian@gmail.com>
2025-03-16 22:27:35 -0500

    step 20

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -84,7 +84,7 @@ public class Main {
                 // System.out.println("formatedArg:"+formatedArg);
                 // System.out.println("redirect:"+redirect);
 
-                handleRedirect(formatedArg, redirect, true);
+                handleRedirect(formatedArg, redirect, "", errRedirect,true);
                 break;
             case "type":
                 if (Arrays.asList(VALID_TYPES).contains(arg)) {
